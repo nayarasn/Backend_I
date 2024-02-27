a0effd6f000149f214db4e6a63e8dde47857524b
Nayara Nascimento <127245980+nayarasn@users.noreply.github.com>
2024-02-27 17:14:10 -0300

    aula_02

---
+++ Workspace_Projeto/api/src/main/java/br/com/alunoonline/api/AlunoOlineApplication.java
@@ -0,0 +1,13 @@
+package br.com.alunoonline.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlunoOlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlunoOlineApplication.class, args);
+	}
+
+}
